a7c98649807251785e254b19b72ecfed726060d2
cyreilv7 <shoiiru3@gmail.com>
2022-05-25 00:01:43 -0700

    Prompt user to update username if first time connecting

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -8,30 +8,36 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./App.scss";
 
-const socket = io.connect("http://localhost:3001", {
-  transports: ["websocket", "polling"], // prevents messages from sitting there and not sending
-});
+const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [username, setUsername] = useState("Anonymous User");
+  const isFirstRender = useRef(true);
   const [users, setUsers] = useState([]);
 
 
   useEffect(() => {
+    console.log("user has entered");
+
     socket.on("connect", () => {
+      console.log("this should be running");
       socket.emit("userEntered", username);
     });
 
     socket.on("users", users => {
       setUsers(users);
-      console.log(users);
     })
 
   }, []);
 
 
   useEffect(() => {
-    socket.emit("usernameChange", username);
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+      socket.emit("usernameChange", username);
+      console.log("new username:", username);
   }, [username]);
 
   return (
@@ -48,8 +54,10 @@ function App() {
               <Col className="submit-container">
                 <Button
                   onClick={() => {
-                    const input = prompt("What's your username?");
-                    setUsername(input);
+                    if (isFirstRender.current) {
+                      const input = prompt("What's your username?");
+                      setUsername(input);
+                    }
                   }}
                   className="submit-btn"
                   variant={"primary"}
@@ -59,13 +67,15 @@ function App() {
               </Col>
             </Row>
           </Col>
-          <Col md={3} className="text-center">
+          <Col md={3} className="text-center d-flex flex-column">
             <h2>Current Users</h2>
-            {users.map(( { username, id } ) => (
-              <Row key={id}>
-                { username }
-              </Row>
-            ))}
+            <div>
+              {users.map(( { username, id } ) => (
+                <div key={id}>
+                  { username }
+                </div>
+              ))}
+            </div>
           </Col>
         </Row>
       </Container>
