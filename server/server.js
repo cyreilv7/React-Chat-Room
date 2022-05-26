eaa078357277dc4fd3e086c5f7525f2f628accf8
cyreilv7 <shoiiru3@gmail.com>
2022-05-25 17:15:15 -0700

    Upload server.js which contains socket.io events

---
+++ server/server.js
@@ -0,0 +1,68 @@
+// set up http server with express
+const express = require("express");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+
+const PORT = 3001 || process.env.PORT;
+server.listen(PORT);
+
+// take care of annoying network issues with cors
+const cors = require("cors");
+app.use(cors()); 
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+  
+const users = {}; // format: { client.id: {username: username, id: client.id}, ... }
+
+// event listeners
+io.on("connection", (socket) => {
+    // when user enters, update userlist for all sockets
+    socket.on("userEntered", (username) => {
+        const user = {
+            username: username,
+            id: socket.id
+        };
+        users[socket.id] = user;
+        io.emit("connected", { 
+            user: user, 
+            msg: {
+                text: `${user.username} has joined the chat.`,
+                date: new Date().toISOString(),
+            } 
+        });
+        io.emit("users", Object.values(users));
+    })
+
+    socket.on("usernameChange", (enteredUsername) => {
+        if (users[socket.id]) {
+            users[socket.id].username = enteredUsername;
+            io.emit("users", Object.values(users));
+        }
+    })
+
+    socket.on("send", message => {
+        io.emit("message", {
+            text: message,
+            date: new Date().toISOString(),
+            user: users[socket.id],
+        });
+    })
+
+    socket.on("disconnect", () => {
+        io.emit("disconnected", {
+            msg: {
+                text: `${users[socket.id].username} has left the chat.`,
+                date: new Date().toISOString(),
+            }, 
+            id: socket.id,
+        });
+        delete users[socket.id]
+    })
+})
+
