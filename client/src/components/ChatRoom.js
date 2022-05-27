5f32273ab18260f817e0c425aa951a163dd81987
cyreilv7 <shoiiru3@gmail.com>
2022-05-27 15:03:37 -0700

    update ChatRoom css

--- client/src/components/ChatRoom.js
+++ client/src/components/ChatRoom.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const ChatRoom = (props) => {
   const rooms = ["general", "random", "programming-help", "career-advice"];
@@ -24,7 +26,9 @@ const ChatRoom = (props) => {
     );
   } else {
     body = (
-      <button onClick={() => props.joinRoom(props.currentChat.chatName)}> Join {props.currentChat.chatName}</button>
+      <div className="d-flex h-100 justify-content-center align-items-center">
+        <Button variant={"primary"} onClick={() => props.joinRoom(props.currentChat.chatName)}> Join {props.currentChat.chatName}</Button>
+      </div>
     )
   }
 
@@ -65,35 +69,39 @@ const ChatRoom = (props) => {
   }
 
   return (
-    <Container className="chatroom-container">
+    <Container className="chatroom-container ms-0">
       <Row style={ { "height": "100%" } }>
-        <Col sm={4} className="sidebar text-center">
-          <Row className="channels">
+        <Col sm={2} className="sidebar text-center border-end">
+          <Row>
             <h3>Channels</h3>
-            {rooms.map(renderRooms)}
+            <Container className="channels d-flex flex-column align-items-center">
+              {rooms.map(renderRooms)}
+            </Container>
           </Row>
-          <Row className="users mt-5">
+          <Row className="mt-5">
             <h3>All users</h3>
-            {props.allUsers.map(renderUsers)}
+            <Container className="users d-flex flex-column align-items-center">
+              {props.allUsers.map(renderUsers)}
+            </Container>
           </Row>
         </Col>
-        <Col sm={8} className="chat-pannel d-flex flex-column">
-          <Row className="channel-info">
-              #{props.currentChat.chatName}
+        <Col sm={9} className="chat-pannel d-flex flex-column">
+          <Row className="channel-info mb-2 border-bottom">
+              <h3>#{props.currentChat.chatName}</h3>
           </Row>
-          <div className="messages">
+          <div className="messages p-3 mb-3 border-bottom">
             { body }
           </div>
-          <div className="text-box-container">
+          <InputGroup className="text-box-container ">
             <textarea
-              className="text-box w-100"
+              className="text-box w-100 form-control text-start"
               value={props.message}
               onChange={props.handleMessageChange}
               onKeyPress={(e) => handleKeyPress(e)}
-              placeholder="Text here"
+              placeholder="Enter message here"
             >
             </textarea>
-          </div>
+          </InputGroup>
         </Col>
       </Row>
 
